1707a299c89c3bbe3d2a52aa18c7be68c4ea2c8d
Your Name <you@example.com>
2025-02-28 13:55:26 +0530

    complete sign up page Functions

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,27 +3,27 @@ import {Link} from 'react-router-dom'
 
 export default function Header() {
   return (
-    <header className='bg-slate-300 shadow-md'>
+    <header className='bg-orange-600 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to= '/'>
       <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-        <span className='text-slate-500'>Royal</span>
-        <span className='text-slate-700'>Estate</span>
+        <span className='text-zinc-50'>Royal</span>
+        <span className='text-slate-950'>Estate</span>
       </h1>
         </Link>
       <form className='bg-slate-100 rounded-lg flex items-center p-2.5'> 
           <input type="text" placeholder=' Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
-          <FaSearch className='text-slate-600'/>
+          <FaSearch className='text-slate-950'/>
       </form>
           <ul className='gap-4  flex'>
             <Link to='/'> 
-            <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+            <li className='hidden sm:inline text-slate-950 font-semibold hover:underline'>Home</li>
             </Link>
-            <Link to='/About'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+            <Link to='/about'>
+            <li className='hidden sm:inline text-slate-950 font-semibold hover:underline'>About</li>
             </Link>
-            <Link to='/Profile'>
-            <li className='text-slate-700 hover:underline'>Signin</li>
+            <Link to='/SignIn'>
+            <li className='text-slate-950 font-semibold hover:underline'>Signin</li>
             </Link>
           </ul>
 
